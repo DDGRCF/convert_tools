e5e116579a6e6644be654e01675a6f2c1f3bc873
DDGRCF <raochaofan1970@gmail.com>
2021-08-17 16:45:15 +0800

    -

--- labels_adjust.py
+++ labels_adjust.py
@@ -8,7 +8,9 @@ from prettytable import PrettyTable
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', type=str, default=r'F:\Datasets\new_annotations')
-    parser.add_argument('--adjust', action='store_true')
+    parser.add_argument('--labels_adjust', action='store_true')
+    parser.add_argument('--paths_adjust', action='store_true')
+    parser.add_argument('--suffix', type=str, default="tif")
     args = parser.parse_args()
 
     dir = Path(args.dir)
@@ -20,19 +22,23 @@ if __name__ == '__main__':
         with open(p, 'rb') as fr:
             label_info = json.load(fr)
         objs = label_info['shapes']
-
+        if args.paths_adjust:
+            i_p = p.stem + "." + args.suffix
+            label_info['imagePath'] = i_p
         for obj in objs:
             label = obj['label']
+            if args.paths_adjust:
+                obj['group_id'] = None
             if label in class_num:
                 class_num[label] += 1
             else:
                 class_num[label] = 1
-            if args.adjust:
+            if args.labels_adjust:
                 if label == '20':
                     obj['label'] = '23'
                 elif label == '23':
                     obj['label'] = '20'
-        if args.adjust:
+        if args.labels_adjust or args.paths_adjust:
             with open(p, 'w') as fw:
                 json.dump(label_info, fw, indent=4)
     for k in sorted(class_num):
